a7a30ff14340d47dcc745b333f4beabdc61dcf7b
Tingan Ho <tingan87@gmail.com>
2013-01-26 00:29:22 +0800

    Added timestamp in deletelog

--- tasks/translate.js
+++ tasks/translate.js
@@ -112,6 +112,7 @@ module.exports = function(grunt) {
             } else {
               deletedTranslations[key][locale] = [];
             }
+            deletedTranslations[key].timestamp = now;
           }
         }
       })(locale);
