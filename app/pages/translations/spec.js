9be929278320c3f76b6f4b773062f6da80ba94e7
Tingan Ho <tingan87@gmail.com>
2013-04-09 17:09:53 +0800

    Removed test

--- app/pages/translations/spec.js
+++ app/pages/translations/spec.js
@@ -2,7 +2,8 @@ var chai    = require('chai'),
     expect  = chai.expect,
     request = require('supertest'),
     cheerio = require('cheerio'),
-    browser = require('zombie');
+    browser = require('zombie'),
+    exec    = require('child_process').exec;
 
 
 var $;
@@ -12,59 +13,16 @@ var $;
 var server = require('../../server').server();
 
 module.exports = function(grunt, options){
-  describe('Interface', function(){
-    describe('Website', function(){
-      it('should return status ok', function(done){
-        request(server)
-        .get('/')
-        .end(function(err, res){
-          if (err) {
-            throw err;
-          }
-          expect(res.statusCode).to.equal(200);
-          done();
-        });
-      });
-    });
-
-    describe('keys', function(){
-      var translations;
-      var keys = [];
-      var bootstrap = require('../../../src/bootstrap');
-
-      before(function(done){
-        translations = grunt.file.readJSON(options.config + '/locales/' + options.defaultLanguage + '.json');
-        for(var key in translations) {
-          if(translations.hasOwnProperty(key)) {
-            keys.push(key);
-          }
-        }
-        keys.sort(function(a, b) {
-          return translations[b].timestamp - translations[a].timestamp;
-        });
-        done();
-      });
-      it('should be ordered correctly', function(done){
-        request(server)
-        .get('/')
-        .end(function(err, res){
-          if(err) {
-            throw err;
-          }
-          $ = cheerio.load(res.text);
-          var n = 0;
-          $('.m-translation-keys li').each(function(){
-            expect($(this).text()).to.equal(keys[n]);
-            n++;
-          });
-          done();
-        });
-      });
-    });
-
+  describe('Interface', function() {
 
     describe('translation values', function() {
       this.timeout(5000);
+      before(function(done) {
+        exec('node app/server.js', function(){
+          done();
+        });
+      });
+
       it('should be able to edit values ', function(done) {
         browser.visit('http://localhost:'
           + server.get('port')
