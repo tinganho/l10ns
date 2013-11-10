1f81f9dabd49858f0404621035d9ce491de97c9e
Tingan Ho <tingan87@gmail.com>
2013-11-10 14:28:15 +0800

    Fixed config settings

--- language/javascript.js
+++
@@ -1,5 +0,0 @@
-
-module.exports = {
-  FUNCTION_CALL_REGEX        : /gt\(['|"](.*)['|"]\s*(\,\s*\{\s*((.*?)|(\s*?))+?\s*\})??\s*\)/g,
-  INNER_FUNCTION_CALL_REGEX  : /\s+(?!gt)[.|\w]+?\((.*?\s*?)*?\)/g
-};
