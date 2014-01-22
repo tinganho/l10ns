35bf1290377a964fee9337d3acc5281b6a601843
Tingan Ho <tingan87@gmail.com>
2014-01-22 21:26:44 +0800

    Added test for Merge#mergeTimeStamp

--- test/test-merger.js
+++ test/test-merger.js
@@ -3,12 +3,31 @@
  * Module dependencies
  */
 
-var fixtures = require('./fixtures/json');
+var fixtures = require('./fixtures/json')
+  , Merger = require('../lib/merger').Merger;
 
 module.exports = function() {
   describe('Merger', function() {
-    describe('#mergeTimeStamp', function() {
+    describe('#constructor', function() {
+      it('should set counter to 0', function() {
+        var merger = new Merger();
+        expect(merger.counter).to.equal(0);
+      });
+    });
 
+    describe('#mergeTimeStamp', function() {
+      it('should use old timestamp', function() {
+        var merger = new Merger();
+        var time = 1000;
+        var res = merger.mergeTimeStamp({ 'test' : {}}, { 'test' : { timestamp : time }}, 'test');
+        expect(res.test.timestamp).to.equal(time);
+      });
+
+      it('should use current timstamp as timestamp whenever old timestamp is undefined', function() {
+        var merger = new Merger();
+        var res = merger.mergeTimeStamp({ 'test' : {}}, { 'test' : {}}, 'test');
+        expect(res.test.timestamp).to.be.a('number');
+      });
     });
   });
 };
