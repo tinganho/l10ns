40735b4ca117cb1d82b85282f6f6627670045818
Tingan Ho <tingan87@gmail.com>
2014-01-23 20:11:40 +0800

    Fixed Search#_createIndex test

--- test/test-search.js
+++ test/test-search.js
@@ -9,28 +9,35 @@ var path = require('path');
  * Import Parser constructor
  */
 
-var Search = require('../lib/search').Search;
+var Search = _Search = require('../lib/search').Search;
 
 module.exports = function() {
   describe('Search', function() {
     describe('#constructor', function() {
+      beforeEach(function() {
+        sinon.stub(_Search.prototype, '_createIndex');
+      });
+
+      afterEach(function() {
+        _Search.prototype._createIndex.restore();
+      });
+
       it('should create index', function() {
-        var _Search = Search;
-        _Search.prototype._createIndex = sinon.spy();
         var search = new _Search();
         search._createIndex.should.have.been.calledOnce;
       });
 
       it('should set default locale from global configs', function() {
-        var _Search = Search;
-        _Search.prototype._createIndex = sinon.spy();
         var search = new _Search();
         expect(search.defaultLocale).to.equal(cf.defaultLocale);
       });
 
+      it('should set index to null', function() {
+        var search = new _Search();
+        expect(search.index).to.not.be.a('undefined');
+      });
+
       it('should set translation to null', function() {
-        var _Search = Search;
-        _Search.prototype._createIndex = sinon.spy();
         var search = new _Search();
         expect(search.translations).to.not.be.a('undefined');
       });
@@ -41,6 +48,28 @@ module.exports = function() {
         _Search.prototype._createIndex = sinon.spy();
         var search = new _Search();
         eventsStub.EventEmitter.should.have.been.calledOnce;
+      });
+    });
+
+    describe('#_createIndex', function() {
+      it('should set the key field', function() {
+        var search = new Search();
+        expect(search.index._fields[0].name).to.equal('key');
+      });
+
+      it('should set the text field', function() {
+        var search = new Search();
+        expect(search.index._fields[1].name).to.equal('text');
+      });
+
+      it('should set the key field to have boost 10', function() {
+        var search = new Search();
+        expect(search.index._fields[0].boost).to.equal(10);
+      });
+
+      it('should set the text field to have boost 10', function() {
+        var search = new Search();
+        expect(search.index._fields[1].boost).to.equal(10);
       });
     });
   });
