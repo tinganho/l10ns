f845e85d025844a59bbb653625ec85bddd13be60
Tingan Ho <tingan87@gmail.com>
2014-01-26 18:10:21 +0800

    Added test for search#query

--- test/test-search.js
+++ test/test-search.js
@@ -32,6 +32,11 @@ module.exports = function() {
         expect(search.defaultLocale).to.equal(cf.defaultLocale);
       });
 
+      it('should set folder from global configs', function() {
+        var search = new _Search;
+        expect(search.folder).to.equal(cf.folder);
+      });
+
       it('should set index to null', function() {
         var search = new _Search;
         expect(search.index).to.not.be.a('undefined');
@@ -132,6 +137,173 @@ module.exports = function() {
 
     describe('#query', function() {
 
+      it('should output `No results found` when nu results are found in search', function() {
+        var logStub = { log : sinon.spy() };
+        var fsStub = { writeFile : sinon.spy() };
+        var Search = proxyquire('../lib/search', { fs : fsStub, './_log' : logStub }).Search;
+        var search = new Search;
+        search.index.add({ id : 'test-id', text : 'test-text', key : 'test-key' });
+        var res = search.query('nottest');
+        logStub.log.should.have.been.calledWith('No result found')
+      });
+
+      it('should slice the search result if the search result is over this.logLength', function() {
+        var logStub = { log : sinon.spy() };
+        var fsStub = { writeFile : sinon.spy() };
+        var Search = proxyquire('../lib/search', { fs : fsStub, './_log' : logStub }).Search;
+        var search = new Search;
+        search.logLength = 1;
+        search.docs = { 'test-ref1' : { key : 'test-key' }};
+        search.index.search = sinon.stub().returns([{
+          id : 'test-id1',
+          ref : 'test-ref1'
+        },
+        {
+          id : 'test-id2',
+          ref : 'test-ref2'
+        }]);
+        search.query('test-query');
+        logStub.log.should.have.callCount(1);
+      });
+
+      it('should format @-tags so that single digit have a space before tag'
+      + ' and double digit don\'t have any space before tag', function() {
+        var logStub = { log : sinon.spy() };
+        var fsStub = { writeFile : sinon.spy() };
+        var Search = proxyquire('../lib/search', {
+          fs : fsStub,
+          './_log' : logStub
+        }).Search;
+        var search = new Search;
+        search.docs = {
+          'test-ref1' : { key : 'test-key1' },
+          'test-ref2' : { key : 'test-key2' },
+          'test-ref3' : { key : 'test-key3' },
+          'test-ref4' : { key : 'test-key4' },
+          'test-ref5' : { key : 'test-key5' },
+          'test-ref6' : { key : 'test-key6' },
+          'test-ref7' : { key : 'test-key7' },
+          'test-ref8' : { key : 'test-key8' },
+          'test-ref9' : { key : 'test-key9' },
+          'test-ref10' : { key : 'test-key10' }
+        };
+        search.index.search = sinon.stub().returns([{
+          id : 'test-id1',
+          ref : 'test-ref1'
+        },
+        {
+          id : 'test-id2',
+          ref : 'test-ref2'
+        },
+        {
+          id : 'test-id3',
+          ref : 'test-ref3'
+        },
+        {
+          id : 'test-id4',
+          ref : 'test-ref4'
+        },
+        {
+          id : 'test-id5',
+          ref : 'test-ref5'
+        },
+        {
+          id : 'test-id6',
+          ref : 'test-ref6'
+        },
+        {
+          id : 'test-id7',
+          ref : 'test-ref7'
+        },
+        {
+          id : 'test-id8',
+          ref : 'test-ref8'
+        },
+        {
+          id : 'test-id9',
+          ref : 'test-ref9'
+        },
+        {
+          id : 'test-id10',
+          ref : 'test-ref10'
+        }]);
+        search.query('test-query');
+        logStub.log.should.have.calledWithMatch(' @1');
+        logStub.log.should.have.calledWithMatch(' @2');
+        logStub.log.should.have.calledWithMatch(' @3');
+        logStub.log.should.have.calledWithMatch(' @4');
+        logStub.log.should.have.calledWithMatch(' @5');
+        logStub.log.should.have.calledWithMatch(' @6');
+        logStub.log.should.have.calledWithMatch(' @7');
+        logStub.log.should.have.calledWithMatch(' @8');
+        logStub.log.should.have.calledWithMatch(' @9');
+        logStub.log.should.have.calledWithMatch('@10');
+      });
+
+      it('should get the search result by mapping the .ref with this.docs', function() {
+        var logStub = { log : sinon.spy() };
+        var fsStub = { writeFile : sinon.spy() };
+        var Search = proxyquire('../lib/search', {
+          fs : fsStub,
+          './_log' : logStub
+        }).Search;
+        var search = new Search;
+        search.docs = {
+          'test-ref1' : { key : 'test-key1' },
+          'test-ref2' : { key : 'test-key2' }
+        };
+        search.index.search = sinon.stub().returns([{
+          id : 'test-id1',
+          ref : 'test-ref1'
+        },
+        {
+          id : 'test-id2',
+          ref : 'test-ref2'
+        }]);
+        search.query('test-query');
+        logStub.log.should.have.calledWithMatch(search.docs['test-ref1'].key);
+        logStub.log.should.have.calledWithMatch(search.docs['test-ref2'].key);
+      });
+
+      it('should write a cache of latest search', function() {
+        var logStub = { log : sinon.spy() };
+        var fsStub = { writeFile : sinon.spy() };
+        var Search = proxyquire('../lib/search', {
+          fs : fsStub,
+          './_log' : logStub
+        }).Search;
+        var search = new Search;
+        search.docs = {
+          'test-ref1' : { key : 'test-key1' },
+          'test-ref2' : { key : 'test-key2' }
+        };
+        search.index.search = sinon.stub().returns([{
+          id : 'test-id1',
+          ref : 'test-ref1'
+        },
+        {
+          id : 'test-id2',
+          ref : 'test-ref2'
+        }]);
+        search.query('test-query');
+        fsStub.writeFile.should.have.been.calledWithMatch('latestSearch.json');
+        expect(fsStub.writeFile.args[0][1]).to.have.a('string');
+      });
+
+      it('should emit `queryend` event when query has ended', function() {
+        var logStub = { log : sinon.spy() };
+        var fsStub = { writeFile : function(path, content, callback) { callback(); } };
+        var Search = proxyquire('../lib/search', {
+          fs : fsStub,
+          './_log' : logStub
+        }).Search;
+        var search = new Search;
+        search.index.search = sinon.stub().returns([]);
+        search.emit = sinon.spy();
+        search.query('test-query');
+        search.emit.should.have.been.calledOnce;
+        search.emit.should.have.been.calledWith('queryend', []);
+      });
     });
   });
 };
