2a90e9e6b62b7fdb0df260d5d1e2f4c836a644d1
Tingan Ho <tingan87@gmail.com>
2014-01-26 14:01:05 +0800

    Fixed terminal colors

--- test/example/example.js
+++ test/example/example.js
@@ -110,5 +110,5 @@ gt('Grunt-translate can have tailing comments with multi-line translation vars',
   test : 'test'
 });  // test1, test2
 
-gt('testbewfaxjgwdefoijiguswefggwgwegexfdwewfwfeefsfefddfweddfwwddwdeedxxd')
-gt('teswfefewwetbafewdewjswdewgdweeefewfwwefxfwddswdewewddv1234')
+gt('wef')
+gt('efwd')
