b6109984a0ceee51fc5b6a881649764d57488002
Tingan Ho <tingan87@gmail.com>
2014-01-27 13:10:33 +0800

    Added test for Edit#_getKey

--- test/test-edit.js
+++ test/test-edit.js
@@ -13,5 +13,35 @@ module.exports = function() {
         expect(edit.defaultLocale).to.equal(cf.defaultLocale);
       }); 
     });
+
+    describe('#_getKey', function() {
+      it('should get key from latest search if it begins with `@`', function() {
+        var edit = new Edit;
+        edit._getKeyFromLatestSearch= sinon.spy();
+        edit._getKey('@1');
+        edit._getKeyFromLatestSearch.should.have.been.calledOnce;
+      });
+
+      it('should get formated key whenver ref begins with `@`', function() {
+        var edit = new Edit;
+        edit._getKeyFromLatestSearch = sinon.spy();
+        edit._getKey('@1');
+        edit._getKeyFromLatestSearch.should.have.been.calledWith('1');
+      });
+
+      it('should get key from latest translations if it begins with `-`', function() {
+        var edit = new Edit;
+        edit._getKeyFromLatestTranslations = sinon.spy();
+        edit._getKey('-1');
+        edit._getKeyFromLatestTranslations.should.have.been.calledOnce;
+        edit._getKeyFromLatestTranslations.should.have.been.calledWith(-1);
+      });
+
+      it('should return the same ref if it isn\'t beginning with `@` or `-`', function() {
+        var edit = new Edit;
+        var res = edit._getKey('test');
+        expect(res).to.equal('test');
+      });
+    });
   });
 };
