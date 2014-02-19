2fecd24f3e7bb9efca4b45a5f43c2b33c780f80c
Tingan Ho <tingan87@gmail.com>
2014-02-19 11:56:25 +0800

    Added Init#constructor test

--- test/test-init.js
+++ test/test-init.js
@@ -25,6 +25,90 @@ module.exports = function() {
         init._createReadlineInterface = sinon.spy();
         expect(init.localesDescription).to.eql(cf.LOCALES_DESCRIPTION);
       });
+
+      it('should set localesSyntax property to the one specified in global config', function() {
+        var init = new Init;
+        init._createReadlineInterface = sinon.spy();
+        expect(init.localesSyntax).to.eql(cf.LOCALES_SYNTAX);
+      });
+
+      it('should set localesWrongAnswer property to the one specified in global config', function() {
+        var init = new Init;
+        init._createReadlineInterface = sinon.spy();
+        expect(init.localesWrongAnswer).to.eql(cf.LOCALES_WRONG_ANSWER);
+      });
+
+      it('should set localesDescription property to the one specified in global config', function() {
+        var init = new Init;
+        init._createReadlineInterface = sinon.spy();
+        expect(init.localesDescription).to.eql(cf.LOCALES_DESCRIPTION);
+      });
+
+      it('should set defaultLocaleCode property to the one specified in global config', function() {
+        var init = new Init;
+        init._createReadlineInterface = sinon.spy();
+        expect(init.defaultLocaleCode).to.eql(cf.DEFAULT_LOCALE_CODE);
+      });
+
+      it('should set defaultLocaleQuestion property to the one specified in global config', function() {
+        var init = new Init;
+        init._createReadlineInterface = sinon.spy();
+        expect(init.defaultLocaleQuestion).to.eql(cf.DEFAULT_LOCALE_QUESTION);
+      });
+
+      it('should set defaultLocaleWrongAnswer property to the one specified in global config', function() {
+        var init = new Init;
+        init._createReadlineInterface = sinon.spy();
+        expect(init.defaultLocaleWrongAnswer).to.eql(cf.DEFAULT_LOCALE_WRONG_ANSWER);
+      });
+
+      it('should set defaultLocaleCode property to the one specified in global config', function() {
+        var init = new Init;
+        init._createReadlineInterface = sinon.spy();
+        expect(init.defaultLocaleCode).to.eql(cf.DEFAULT_LOCALE_CODE);
+      });
+
+      it('should set programmingLanguages property to the one specified in global config', function() {
+        var init = new Init;
+        init._createReadlineInterface = sinon.spy();
+        expect(init.programmingLanguages).to.eql(cf.PROGRAMMING_LANGUAGUES);
+      });
+
+      it('should set chooseProgrammingLanguagePrompt property to the one specified in global config', function() {
+        var init = new Init;
+        init._createReadlineInterface = sinon.spy();
+        expect(init.chooseProgrammingLanguagePrompt).to.eql(cf.CHOOSE_PROGRAMMING_LANGUAGE_PROMPT);
+      });
+
+      it('should set chooseProgrammingLanguageWrongAnswer property to the one specified in global config', function() {
+        var init = new Init;
+        init._createReadlineInterface = sinon.spy();
+        expect(init.chooseProgrammingLanguageWrongAnswer).to.eql(cf.CHOOSE_PROGRAMMING_LANGUAGE_WRONG_ANSWER);
+      });
+
+      it('should set programmingLanguageToDefaultSrcMap property to the one specified in global config', function() {
+        var init = new Init;
+        init._createReadlineInterface = sinon.spy();
+        expect(init.programmingLanguageToDefaultSrcMap).to.eql(cf.PROGRAMMING_LANGUAGUE_TO_DEFAULT_SRC_MAP);
+      });
+
+      it('should set defaultOutputFolder property to the one specified in global config', function() {
+        var init = new Init;
+        init._createReadlineInterface = sinon.spy();
+        expect(init.defaultOutputFolder).to.eql(cf.DEFAULT_OUTPUT_FOLDER);
+      });
+
+      it('should set defaultOutputFolderPrompt property to the one specified in global config', function() {
+        var init = new Init;
+        init._createReadlineInterface = sinon.spy();
+        expect(init.defaultOutputFolderPrompt).to.eql(cf.DEFAULT_OUTPUT_FOLDER_PROMPT);
+      });
+
+      it('should set defaultOutputFolderWrongAnswer property to the one specified in global config', function() {
+        var init = new Init;
+        init._createReadlineInterface = sinon.spy();
+        expect(init.defaultOutputFolderWrongAnswer).to.eql(cf.DEFAULT_OUTPUT_FOLDER_WRONG_ANSWER);
+      });
     });
 
     describe('#init', function() {
