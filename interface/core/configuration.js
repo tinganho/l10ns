0649401b2e48542d31ba298118f9f3f6ca50416e
Tingan Ho <tingan87@gmail.com>
2014-05-06 15:39:22 +0800

    Fixed name space typo

--- interface/core/configuration.js
+++ interface/core/configuration.js
@@ -96,7 +96,7 @@ Config.prototype.writeClientConfigurations = function() {
     for(var i = 0; i < matches.length; i++) {
       var configurations = require(cf.ROOT_FOLDER + matches[i]);
 
-      var startWrap  = 'window.' + configurations.NAMESPACE + ' = (function() { var configs = '
+      var startWrap  = 'window.' + configurations.NAME_SPACE + ' = (function() { var configs = '
         , body       = JSON.stringify(configurations, null, 2) + ';'
         , makeRegExp = 'for(var key in configs) { if(/REGEX/.test(key)) { configs[key] = new RegExp(configs[key]); } }'
         , endWrap    = 'return configs; })();';
