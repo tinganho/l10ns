c134480db63bfc8ceac9a1261e1bd31e752ba1f3
Tingan Ho <tingan87@gmail.com>
2014-07-19 18:16:45 +0800

    Added test for Update#update

--- specifications/update.js
+++ specifications/update.js
@@ -7,20 +7,22 @@ global.pcf = {
 
 };
 
-var Update = proxyquire('../libraries/update', {
+var dependencies = {
   'fs': {},
+  'readline': {},
+  'underscore': {},
+  'path': {},
   './parser': {},
   './syntax': {},
   './merger': {},
   './file': {},
-  'readline': {},
-  'underscore': {},
-  path: {} }).Update;
+  './_log': {}
+};
 
 describe('update', function() {
   describe('#update', function() {
     it('should get the source keys', function() {
-      var update = new Update;
+      var update = new (proxyquire('../libraries/update', dependencies).Update);
       update._getSourceKeys = sinon.spy();
       update._mergeTranslations = function() {};
       update.update();
@@ -28,12 +30,32 @@ describe('update', function() {
     });
 
     it('should merge source keys with old translations', function() {
-      var update = new Update;
+      var update = new (proxyquire('../libraries/update', dependencies).Update);
       update._getSourceKeys = function() {};
       update._mergeTranslations = sinon.spy();
       update.update();
       update._mergeTranslations.should.have.been.calledOnce;
+    });
 
+    it('should write all the translation to storage', function() {
+      dependencies['./file'].writeTranslations = sinon.spy();
+      var update = new (proxyquire('../libraries/update', dependencies).Update);
+      update._getSourceKeys = function() {};
+      update._mergeTranslations = sinon.stub().callsArgWith(1, null, {});
+      update.update();
+      dependencies['./file'].writeTranslations.should.have.been.calledOnce;
+      dependencies['./file'].writeTranslations.should.have.been.calledWith({});
+    });
+
+    it('should log an error if merge translations have been failed', function() {
+      dependencies['./file'].writeTranslations = function() {};
+      dependencies['./_log'].error = sinon.spy();
+      var update = new (proxyquire('../libraries/update', dependencies).Update);
+      update._getSourceKeys = function() {};
+      update._mergeTranslations = sinon.stub().callsArgWith(1, new Error('test'), {});
+      update.update();
+      dependencies['./_log'].error.should.have.been.calledOnce;
+      dependencies['./_log'].error.should.have.been.calledWith('Translation update failed');
     });
   });
 });
