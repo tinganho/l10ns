c8246d25572d9318bbe1934f85b2fb19edf44866
Tingan Ho <tingan87@gmail.com>
2014-08-14 14:34:49 +0800

    Added output example

---
+++ example/javascript/output/localizations.js
@@ -0,0 +1,53 @@
+;(function() {
+  var localizations = {
+    'en-US': {
+      'lo\'\"cale1': function anonymous(it) {
+        return 'hej';
+      },
+      'locale2': function anonymous(it) {
+        return 'KEY_NOT_TRANSLATED: locale2';
+      }
+    },
+    'zh-CN': {
+      'lo\'\"cale1': function anonymous(it) {
+        return 'hwefpiojewfij';
+      },
+      'locale2': function anonymous(it) {
+        return 'KEY_NOT_TRANSLATED: locale2';
+      }
+    }
+  };
+  
+  function requireLocale(locale) {
+    return (function(locale) {
+      return function l(key) {
+        if(!(locale in localizations)) {
+          return 'LOCALE_NOT_IN_LOCALIZATIONS: ' + locale;
+        }
+        if(!(key in localizations[locale])) {
+          return 'KEY_NOT_IN_LOCALIZATIONS: ' + key;
+        }
+        return localizations[locale][key].call(undefined, arguments[1]);
+      };
+    })(locale);
+  };
+  
+  function lci(operand1, operand2) {
+    operand1 = operand1 + '';
+    operand2 = operand2 + '';
+    operand1LastNumber = operand1.substr(-1,1);
+    return operand1LastNumber === operand2;
+  };
+
+  if(typeof require === "function" && typeof exports === 'object' && typeof module === 'object') {
+    module.exports = requireLocale;
+  }
+  else if (typeof define === "function" && define.amd) {
+    define(function() {
+      return requireLocale;
+    });
+  }
+  else {
+    window.requireLocale = requireLocale;
+  }
+})();
