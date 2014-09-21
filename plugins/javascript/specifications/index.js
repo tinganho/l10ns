1b3046dc4afdbd2ba6d631c830cb63cce1f25894
Tingan Ho <tingan87@gmail.com>
2014-09-21 12:04:02 +0800

    Fixes exponent number formating

--- plugins/javascript/specifications/index.js
+++ plugins/javascript/specifications/index.js
@@ -212,8 +212,15 @@ describe('Javascript Compiler', function() {
     var symbols = {
       decimal: '.',
       group: ',',
-      percent: '%',
-      permille: '‰'
+      list: ';',
+      percentSign: '%',
+      plusSign: '+',
+      minusSign: '-',
+      exponential: 'E',
+      superscriptingExponent: '×',
+      perMille: '‰',
+      infinity: '∞',
+      nan: 'NaN'
     };
     describe('formatNumber()', function() {
       it('should be able to group primary', function() {
@@ -616,7 +623,7 @@ describe('Javascript Compiler', function() {
         expect(number).to.equal('$10');
       });
 
-      it('should be able to append a space if a non-currency symbol surrounfs a digit', function() {
+      it('should be able to append a space if a non-currency symbol surrounds a digit', function() {
         var number = formatNumber({
           prefix: '¤',
           suffix: '',
@@ -640,7 +647,7 @@ describe('Javascript Compiler', function() {
         expect(number).to.equal('$NT 10');
       });
 
-      it('should not append a space if a non-currency symbol surrounfs a digit', function() {
+      it('should not append a space if a non-currency symbol surrounds a digit', function() {
         var number = formatNumber({
           prefix: '¤',
           suffix: '',
@@ -664,7 +671,7 @@ describe('Javascript Compiler', function() {
         expect(number).to.equal('$10');
       });
 
-      it('should be able to prepend a space if a non-currency symbol surrounfs a digit', function() {
+      it('should be able to prepend a space if a non-currency symbol surrounds a digit', function() {
         var number = formatNumber({
           prefix: '',
           suffix: '¤',
@@ -686,30 +693,6 @@ describe('Javascript Compiler', function() {
           symbols: symbols
         });
         expect(number).to.equal('10 US$');
-      });
-
-      it('should not prepend a space if a non-currency symbol surrounds a digit', function() {
-        var number = formatNumber({
-          prefix: '',
-          suffix: '¤',
-          roundTo: 1,
-          number: 10,
-          percentage: false,
-          permille: false,
-          currency: {
-            symbol: '$'
-          },
-          maximumFractionDigits: 0,
-          minimumFractionDigits: 0,
-          minimumIntegerDigits: 1,
-          type: 'floating',
-          groupSize: {
-            primary: 3,
-            secondary: 2
-          },
-          symbols: symbols
-        });
-        expect(number).to.equal('10$');
       });
 
       it('should not prepend a space if a non-currency symbol surrounds a digit', function() {
@@ -1046,6 +1029,235 @@ describe('Javascript Compiler', function() {
           symbols: symbols
         });
         expect(number).to.equal('7,89,000‰');
+      });
+
+      it('should be able to format an exponent number with only minimum digits specified', function() {
+        var number = formatNumber({
+          type: 'floating',
+          prefix: '',
+          suffix: '',
+          roundTo: 1,
+          number: 12000,
+          percentage: false,
+          permille: false,
+          currency: null,
+          exponent: {
+            digits: 1,
+            plusSign: false
+          },
+          maximumFractionDigits: 0,
+          minimumFractionDigits: 0,
+          minimumIntegerDigits: 2,
+          maximumIntegerDigits: 2,
+          maximumSignificantDigits: 3,
+          minimumSignificantDigits: 3,
+          groupSize: {
+            primary: 3,
+            secondary: 2
+          },
+          symbols: symbols
+        });
+        expect(number).to.equal('12E3');
+      });
+
+      it('should be able to format an exponent number with exponent grouping(maximum digit specified)', function() {
+        var number = formatNumber({
+          type: 'floating',
+          prefix: '',
+          suffix: '',
+          roundTo: 1,
+          number: 12345,
+          percentage: false,
+          permille: false,
+          currency: null,
+          exponent: {
+            digits: 1,
+            plusSign: false
+          },
+          maximumFractionDigits: 4,
+          minimumFractionDigits: 0,
+          minimumIntegerDigits: 1,
+          maximumIntegerDigits: 3,
+          maximumSignificantDigits: 3,
+          minimumSignificantDigits: 3,
+          groupSize: {
+            primary: 3,
+            secondary: 2
+          },
+          symbols: symbols
+        });
+        expect(number).to.equal('12.345E3');
+      });
+
+      it('should be able to format an exponent number with plus sign', function() {
+        var number = formatNumber({
+          type: 'floating',
+          prefix: '',
+          suffix: '',
+          roundTo: 1,
+          number: 100320,
+          percentage: false,
+          permille: false,
+          currency: null,
+          exponent: {
+            digits: 1,
+            plusSign: true
+          },
+          maximumFractionDigits: 3,
+          minimumFractionDigits: 0,
+          minimumIntegerDigits: 1,
+          maximumIntegerDigits: 3,
+          maximumSignificantDigits: 3,
+          minimumSignificantDigits: 3,
+          groupSize: {
+            primary: 3,
+            secondary: 2
+          },
+          symbols: symbols
+        });
+        expect(number).to.equal('100.3E+3');
+      });
+
+      it('should be able to format an exponent number with negative exponent', function() {
+        var number = formatNumber({
+          type: 'floating',
+          prefix: '',
+          suffix: '',
+          roundTo: 1,
+          number: 0.0087,
+          percentage: false,
+          permille: false,
+          currency: null,
+          exponent: {
+            digits: 1,
+            plusSign: true
+          },
+          maximumFractionDigits: 1,
+          minimumFractionDigits: 0,
+          minimumIntegerDigits: 1,
+          maximumIntegerDigits: 1,
+          maximumSignificantDigits: 3,
+          minimumSignificantDigits: 3,
+          groupSize: {
+            primary: 3,
+            secondary: 2
+          },
+          symbols: symbols
+        });
+        expect(number).to.equal('8.7E-3');
+      });
+
+      it('should be able to format an exponent number with specififed significant digits', function() {
+        var number = formatNumber({
+          type: 'significant',
+          prefix: '',
+          suffix: '',
+          roundTo: 1,
+          number: 0.0087,
+          percentage: false,
+          permille: false,
+          currency: null,
+          exponent: {
+            digits: 1,
+            plusSign: true
+          },
+          maximumFractionDigits: 1,
+          minimumFractionDigits: 0,
+          minimumIntegerDigits: 1,
+          maximumIntegerDigits: 1,
+          maximumSignificantDigits: 2,
+          minimumSignificantDigits: 2,
+          groupSize: {
+            primary: 3,
+            secondary: 2
+          },
+          symbols: symbols
+        });
+        expect(number).to.equal('8.7E-3');
+      });
+
+      it('should be able to format an exponent number with specififed significant digits by appending trailing zeros', function() {
+        var number = formatNumber({
+          type: 'significant',
+          prefix: '',
+          suffix: '',
+          roundTo: 1,
+          number: 0.00000087,
+          percentage: false,
+          permille: false,
+          currency: null,
+          exponent: {
+            digits: 1,
+            plusSign: true
+          },
+          maximumFractionDigits: 1,
+          minimumFractionDigits: 0,
+          minimumIntegerDigits: 1,
+          maximumIntegerDigits: 1,
+          maximumSignificantDigits: 3,
+          minimumSignificantDigits: 3,
+          groupSize: {
+            primary: 3,
+            secondary: 2
+          },
+          symbols: symbols
+        });
+        expect(number).to.equal('8.70E-7');
+      });
+
+      it('should use the specified number of exponent digit before maximum integer digits', function() {
+        var number = formatNumber({
+          type: 'floating',
+          prefix: '',
+          suffix: '',
+          roundTo: 1,
+          number: 0.00000000087,
+          percentage: false,
+          permille: false,
+          currency: null,
+          exponent: {
+            digits: 1,
+            plusSign: true
+          },
+          maximumFractionDigits: 1,
+          minimumFractionDigits: 0,
+          minimumIntegerDigits: 1,
+          maximumIntegerDigits: 1,
+          maximumSignificantDigits: 3,
+          minimumSignificantDigits: 3,
+          groupSize: {
+            primary: 3,
+            secondary: 2
+          },
+          symbols: symbols
+        });
+        expect(number).to.equal('0.87E-9');
+        var number = formatNumber({
+          type: 'floating',
+          prefix: '',
+          suffix: '',
+          roundTo: 1,
+          number: 87000000000,
+          percentage: false,
+          permille: false,
+          currency: null,
+          exponent: {
+            digits: 1,
+            plusSign: false
+          },
+          maximumFractionDigits: 1,
+          minimumFractionDigits: 0,
+          minimumIntegerDigits: 1,
+          maximumIntegerDigits: 1,
+          maximumSignificantDigits: 3,
+          minimumSignificantDigits: 3,
+          groupSize: {
+            primary: 3,
+            secondary: 2
+          },
+          symbols: symbols
+        });
+        expect(number).to.equal('87E9');
       });
     });
   });
