222db9209fa2b4e1b8780fd4c449d0436adfd22d
Tingan Ho <tingan87@gmail.com>
2014-11-12 17:56:59 +0800

    Adds test case about a variable postceeding a linefeed

--- plugins/javascript/specifications/sentenceCompilation.js
+++ plugins/javascript/specifications/sentenceCompilation.js
@@ -115,6 +115,25 @@ describe('Sentences', function() {
     });
   });
 
+  it('should be able to compile a sentence with a line feed and a variable postceeding it', function(done) {
+    var localizations = getLocalizations('sentence1\n{variable1}')
+      , dependencies = getDependencies(localizations)
+      , compiler = proxyquire('../plugins/javascript/compiler', dependencies);
+
+    compiler.run();
+    eventually(function() {
+      var functionBody =
+      'var string = \'\';\n' +
+      'string += \'sentence1\\n\';\n' +
+      'string += it.variable1;\n' +
+      'return string;';
+      expect(dependencies.fs.writeFileSync.args[1][1]).to.eql(template['JavascriptWrapper']({
+        functionBody: indentSpaces(8, functionBody)
+      }));
+      done();
+    });
+  });
+
   it('should be able to compile a sentence with a space', function(done) {
     var localizations = getLocalizations('sentence1 ')
       , dependencies = getDependencies(localizations)
