5930ec4f8e63d97ba2f8a93b7513930954df4181
Tingan Ho <tingan87@gmail.com>
2015-02-08 00:31:09 +0800

    fixes ga report

--- binaries/track.js
+++ binaries/track.js
@@ -3,7 +3,7 @@ var package = require('../package.json');
 var cmd = process.argv[2];
 var Insight = require('anonymous-insight');
 var insight = new Insight({
-  trackingCode: 'UA-51369650-4',
+  trackingCode: process.env.L10NS_DEV || 'UA-51369650-4',
   pkg: package
 });
 
