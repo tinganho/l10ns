b8098c7c263dcc913b60544eedd60bc154da234c
Tingan Ho <tingan87@gmail.com>
2015-03-07 11:13:41 +0800

    Adds different GA ID

--- binaries/install-report.js
+++ binaries/install-report.js
@@ -3,7 +3,7 @@ var spawn = require('child_process').spawn;
 var path = require('path');
 
 function track(cmd) {
-  spawn(process.execPath, [path.join(__dirname, 'track.js'), cmd], {
+  spawn(process.execPath, [path.join(__dirname, 'track.js'), cmd, 'UA-51369650-5'], {
     detached: true,
     stdio: 'ignore'
   }).unref();
