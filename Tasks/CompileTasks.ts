697364a029b025df3c7a52befcfad5db8f81e887
Tingan Ho <tingan87@gmail.com>
2016-09-17 23:58:40 +0800

    Fixes formatting issues

--- Tasks/CompileTasks.ts
+++ Tasks/CompileTasks.ts
@@ -1,14 +1,14 @@
 import { exec } from 'child_process';
 
 function run(grunt: IGrunt) {
-	grunt.registerTask('compile-tasks', (done: () => void) => {
-		const cmd = 'cat Build/Tasks/GenerateDiagnostics.js Build/Binaries/L10ns.js > Build/Tasks/GenerateDiagnostics.Bundle.js';
-		console.log(cmd);
+    grunt.registerTask('compile-tasks', (done: () => void) => {
+        const cmd = 'cat Build/Tasks/GenerateDiagnostics.js Build/Binaries/L10ns.js > Build/Tasks/GenerateDiagnostics.Bundle.js';
+        console.log(cmd);
         exec(cmd, (_err, stdout, _) => {
             console.log(stdout);
             done();
         });
-	});
+    });
 }
 
 module.exports = run;
