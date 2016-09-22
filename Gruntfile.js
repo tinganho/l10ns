e72986afdebb4266eb8f8b13e55254b8ff89d567
Tingan Ho <tingan87@gmail.com>
2016-09-22 17:47:05 +0800

    Fixes travis issue

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 
-global.L10ns = require('./Build/Source/Service/System').L10ns;
+let hasCompiledSource = false;
+try {
+    fs.lstatSync(path.join(__dirname, 'Build/Source/Service/System'));
+    hasCompiledSource = true;
+}
+catch(err) {}
+if (hasCompiledSource) {
+    global.L10ns = require('./Build/Source/Service/System').L10ns;
+}
 
 function setup(grunt) {
     grunt.loadTasks('Build/Tasks');
