8903d3d2a516b36bf4a8bc21d9fafdb321e1f4a4
Tingan Ho <tingan87@gmail.com>
2017-03-04 18:13:38 +0100

    Adds Diagnostic

--- src/Program/Diagnostics.cpp
+++ src/Program/Diagnostics.cpp
@@ -4,8 +4,10 @@
 #include "Types.cpp"
 using namespace std;
 namespace D {
-    auto Missing_field_in_your_extension_file = new DiagnosticTemplate("Missing field '{0}' in your extension file: '{1}'.");
-    auto Unknown_capability = new DiagnosticTemplate("Unknown capability '{0}'.");
+    auto Could_not_find_an_extension_for_the_programming_language = new DiagnosticTemplate("Could not find an extension for the programming language '{0}'.");
+    auto Could_not_start_extension_server = new DiagnosticTemplate("Could not start extension server.");
+    auto Missing_field_in_your_extension_file = new DiagnosticTemplate("Missing field '{0}' in your extension file '{1}'.");
+    auto Unknown_capability_in_extension_file = new DiagnosticTemplate("Unknown capability '{0}' in extension file '{1}'.");
     auto Unknown_command = new DiagnosticTemplate("Unknown command '{0}'.");
     auto Unknown_command_flag = new DiagnosticTemplate("Unknown command flag '{0}'.");
     auto You_are_not_inside_a_L10ns_project = new DiagnosticTemplate("You are not inside a L10ns project.");
